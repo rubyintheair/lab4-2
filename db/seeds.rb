406b85de1f5405cecd920faa7c5680c214ff77e1
yennguyen79 <yen.nnh794@gmail.com>
2017-08-01 16:12:23 +0700

    coomit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Seeding.rb"
+if Photo.count > 0
+    print "WARNING: there is existing data."
+    puts "To delete all and seed again, run: FORCE=1 rails db:seed"
+    exit
+end
+# Photo.destroy_all
+
+Photo.create!(url: "http://idequals.com/images/business_header_images/la_costa_header.png", username: "La costa", likes_count: 1234)
+Photo.create!(url: "http://3.bp.blogspot.com/-OSweHsbV96U/TvlPfVdyulI/AAAAAAAAIn4/EVzQ0lEP9Qc/s1600/6246553_orig.jpg", username: "mexican restaurand", likes_count: 232)
+Photo.create!(url: "http://s3-media4.fl.yelpcdn.com/bphoto/HTVTW8lechuGzhZLRG52rA/ls.jpg", username: "Mexican food", likes_count: 11)
+
+puts "Seeded #{Photo.count} photos. "
+10.times do
+  username = Faker::HowIMetYourMother.character
+  caption  = Faker::HowIMetYourMother.quote
+  url = Faker::LoremPixel.image("500x500", false, 'sports', nil, username)
+  Photo.create!(url: url, username: username,
+                caption: caption, likes_count: Faker::Number.number(5))
+end
